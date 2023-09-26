092d6fa7b3f59fb1bef03afe9099adec2ef16591
Mario756 <mariomazziotti756@gmail.com>
2023-09-26 17:35:52 +0200

    addded the first fragment with some logic

---
+++ app/src/main/java/com/example/calculator_cf/AppViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.calculator_cf
+
+import android.view.LayoutInflater
+import androidx.lifecycle.ViewModel
+import com.example.calculator_cf.databinding.FragmentHomeBinding
+
+class AppViewModel : ViewModel() {
+
+//Fragment_Home Logic
+fun exit_app(){
+
+}
+
+
+
+}
