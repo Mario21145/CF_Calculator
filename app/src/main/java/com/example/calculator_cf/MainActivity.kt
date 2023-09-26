a82c837f3fb9df7ac2f0333710fe20ab085b0b36
Mario756 <mariomazziotti756@gmail.com>
2023-09-26 17:35:52 +0200

    addded the first fragment with some logic

--- app/src/main/java/com/example/calculator_cf/MainActivity.kt
+++ app/src/main/java/com/example/calculator_cf/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.calculator_cf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.calculator_cf.databinding.ActivityMainBinding
+import com.example.calculator_cf.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,9 +22,9 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var binding_navigation_graph = findViewById<View>(R.id.nav_host_fragment)
-        val navController = binding_navigation_graph.findNavController()
-
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        val navController = navHostFragment?.findNavController()
+        //Log.d("navcontroller" , "$navController")
     }
 
 
